227c070a121685458d1064211ca69c8a4070ac00
Patrick Vuong <patrickv77@gmail.com>
2024-02-13 15:01:21 -0600

    index js boilerplate

--- client/index.js
+++ client/index.js
@@ -1,2 +1,6 @@
-//added for initial file structure
-//entry point for react
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+// import App from './components/App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(<App />);
