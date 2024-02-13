75364d72a10ab3438711a778ec2bcd121630d6b9
Patrick Vuong <patrickv77@gmail.com>
2024-02-13 15:03:35 -0600

    create components folder, create app file aka landing page

---
+++ client/components/App.js
@@ -0,0 +1,14 @@
+// front page of the app
+
+import React from 'react';
+
+const App = () => {
+  return (
+    <div className="main">
+      <h1>TAB SPLITTER</h1>
+      <body>lorem ipsum etc</body>
+    </div>
+  );
+};
+
+export default App;
