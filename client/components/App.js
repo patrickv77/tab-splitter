8b1cd5d0f5cc3aae85e48eb7fb5111660f4a1716
Patrick Vuong <patrickv77@gmail.com>
2024-02-14 15:22:19 -0600

    building out file structure

--- client/components/App.js
+++ client/components/App.js
@@ -4,8 +4,8 @@ import React from 'react';
 
 const App = () => {
   return (
-    <div>
-      <h1 className="bg-slate-500">TAB SPLITTER</h1>
+    <div className="flex flex-col h-screen border-2 border-solid border-orange-400">
+      <h1 className="bg-slate-500 text-center">TAB SPLITTER</h1>
       <body className="bg-red-900 text-white">
       lorem ipsum etc asdf
       </body>
@@ -21,5 +21,10 @@ export default App;
 /*
 
 todo: design the UI
+
+route to pages, store components of pages in components
+
+or route to pages and store components of pages in [PageName]Components folder?
+
 
 */
