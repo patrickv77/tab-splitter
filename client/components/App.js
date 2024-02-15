53b8d7c8e45fca5794b9d4df6099c38ae5220c81
Patrick Vuong <patrickv77@gmail.com>
2024-02-15 14:57:32 -0600

    flex tinkering

--- client/components/App.js
+++ client/components/App.js
@@ -5,9 +5,10 @@ import React from 'react';
 const App = () => {
   return (
     <div className="flex flex-col h-screen border-2 border-solid border-orange-400">
-      <h1 className="flex bg-slate-500 flex-1 items-center justify-center max-h-[80px]">
-        TAB SPLITTER
-      </h1>
+      <div className="flex flex-col bg-slate-500 flex-1 items-center justify-center max-h-[80px] ">
+        <h1>TAB SPLITTER</h1>
+        <p>hello world</p>
+      </div>
       <div className="flex bg-red-900 text-white flex-1 items-center justify-center">landing page</div>
     </div>
   );
