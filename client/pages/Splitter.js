0138bf4a68b0d9db6e176635d7f3f757060ff03d
Patrick Vuong <patrickv77@gmail.com>
2024-02-15 15:41:24 -0600

    ideas for splitter page

--- client/pages/Splitter.js
+++ client/pages/Splitter.js
@@ -1,1 +1,22 @@
 // placeholder file for main app functionality page
+
+/*
+
+  main functionality page
+
+  two main columns:
+  left column (larger) will be items
+  right column (much smaller) will be users
+
+  drag and drop users into items, items will split automatically based on the amount of users that exist on that item
+
+  notes:
+  lock items to reduce visual clutter? <-- move to a separate tab that is collapsable
+
+  where to put tax and tip options?
+
+  full summary page to see how much each person owes and a way to indicate who paid maybe
+
+  code to join room? lock maneuverability of users to user and room creator?
+
+*/
