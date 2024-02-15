d7f542bc97e0e5fb867d8b24eebb1dd0f1970291
Patrick Vuong <patrickv77@gmail.com>
2024-02-15 15:21:51 -0600

    ideas for ticket page

--- client/pages/Tickets.js
+++ client/pages/Tickets.js
@@ -1,1 +1,8 @@
 // placeholder file for a page that shows all tickets (splitter pages) past and present
+
+/*
+  filtering options, present and past tickets
+
+  carousel of tickets, click a ticket to view
+
+*/
