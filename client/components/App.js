4bfa8ddfb1ca32a575b258eff653c7fd092807a7
Patrick Vuong <patrickv77@gmail.com>
2024-02-15 15:11:38 -0600

    move app to home.js, import home into app

--- client/components/App.js
+++ client/components/App.js
@@ -1,16 +1,11 @@
 // front page of the app
 
 import React from 'react';
+import Home from '../pages/Home';
 
 const App = () => {
   return (
-    <div className="flex flex-col h-screen border-2 border-solid border-orange-400">
-      <div className="flex flex-col bg-slate-500 flex-1 items-center justify-center max-h-[80px] ">
-        <h1>TAB SPLITTER</h1>
-        <p>hello world</p>
-      </div>
-      <div className="flex bg-red-900 text-white flex-1 items-center justify-center">landing page</div>
-    </div>
+    <Home />
   );
 };
 
--- client/pages/Home.js
+++ client/pages/Home.js
@@ -1,1 +1,16 @@
 // placeholder file for app landing page, could potentially contain login box as well
+
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div className="flex flex-col h-screen border-2 border-solid border-orange-400">
+      <div className="flex flex-col bg-slate-500 flex-1 items-center justify-center max-h-[80px] ">
+        <h1 className="text-3xl font-bold">TAB SPLITTER</h1>
+      </div>
+      <div className="flex bg-red-900 text-white flex-1 items-center justify-center">landing page</div>
+    </div>
+  );
+};
+
+export default Home;
