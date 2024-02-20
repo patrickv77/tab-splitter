34dd93e7c49d3834afddc138388b5ea332dc1a26
Patrick Vuong <patrickv77@gmail.com>
2024-02-20 16:28:09 -0600

    add margin to tab item elements

--- client/components/TabItem.js
+++ client/components/TabItem.js
@@ -4,7 +4,10 @@ import React from 'react';
 
 const TabItem = () => {
   return (
-    <div className="flex-none h-24 border-2 border-solid border-red-600">this is where the bill elements will live, should be able to add, remove, edit, etc</div>
+    <div className="flex-none h-24 border-2 border-solid border-red-600 rounded-xl my-[1px]">
+      this is where the bill elements will live, should be able to add, remove,
+      edit, etc
+    </div>
   );
 };
 
