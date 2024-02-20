583185d7dfc89958c5cb10306835367453e76a48
Patrick Vuong <patrickv77@gmail.com>
2024-02-20 15:03:37 -0600

    move tab item from splitter to tabitem

--- client/components/TabItem.js
+++ client/components/TabItem.js
@@ -4,12 +4,7 @@ import React from 'react';
 
 const TabItem = () => {
   return (
-    <div className="flex flex-col h-screen border-2 border-solid border-orange-400">
-      <div className="flex flex-col bg-slate-500 flex-1 items-center justify-center max-h-[80px] ">
-        <h1 className="text-3xl font-bold">TAB SPLITTER</h1>
-      </div>
-      <div className="flex bg-red-900 text-white flex-1 items-center justify-center italic">landing page</div>
-    </div>
+    <div className="flex-none h-24 border-2 border-solid border-red-600">this is where the bill elements will live, should be able to add, remove, edit, etc</div>
   );
 };
 
