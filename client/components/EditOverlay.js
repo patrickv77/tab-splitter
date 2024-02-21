de57eaed737f34f27dd699d8c9843c2139dfbfee
Patrick Vuong <patrickv77@gmail.com>
2024-02-21 14:42:58 -0600

    add edit overlay boilerplate and TODO list

---
+++ client/components/EditOverlay.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const EditOverlay = () => {
+  return (
+    <div className="flex-none h-24 border-2 border-solid border-red-600 rounded-xl my-[1px]">
+      when you click to add an item or click to edit an item, this will pop up and allow you to edit the values
+    </div>
+  );
+}
+
+export default EditOverlay;
+
+// TODO:
+/*
+
+  figure out how to make this work as intended!!
+  pop up when clicked on
+  required input boxes!! name and price
+  disappear when done editing
+
+ */
