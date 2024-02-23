974bb36d1c716997b47329233a10652c75579760
Patrick Vuong <patrickv77@gmail.com>
2024-02-23 14:37:38 -0600

    create edit button in tab item component

--- client/components/TabItem.js
+++ client/components/TabItem.js
@@ -4,9 +4,10 @@ import React from 'react';
 
 const TabItem = () => {
   return (
-    <div className="flex-none h-24 border-2 border-solid border-red-600 rounded-xl my-[1px]">
+    <div className="flex flex-none h-24 border-2 border-solid border-red-600 rounded-xl my-[1px]">
       this is where the bill elements will live, should be able to add, remove,
       edit, etc
+      <button className="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none font-medium text-sm rounded-lg px-5 py-2.5 text-center mr-5">EDIT</button>
     </div>
   );
 };
