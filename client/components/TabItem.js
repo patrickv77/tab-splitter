ba80fcd4aad51a9961a682137f896ab5cb23a02a
Patrick Vuong <patrickv77@gmail.com>
2024-03-01 14:39:38 -0600

    align edit/delete buttons to the right

--- client/components/TabItem.js
+++ client/components/TabItem.js
@@ -5,12 +5,14 @@ import React from 'react';
 const TabItem = () => {
   return (
     <div className="flex flex-none h-24 border-2 border-solid border-red-600 rounded-xl my-[1px]">
-      <div className="grow-1 w-1/6">[item name]</div>
-      <div className="grow-1 w-1/6">[item price]</div>
-      <div className="flex grow-1 w-2/3">
-        <button className="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none font-medium text-sm rounded-lg px-5 py-2.5 text-center mr-5">EDIT</button>
-        <button className="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none font-medium text-sm rounded-lg px-5 py-2.5 text-center mr-5">DELETE</button>
+      <div className="flex items-center justify-center grow-1 w-1/6">[item name]</div>
+      <div className="flex items-center justify-center grow-1 w-1/6">[item price]</div>
+      <div className="flex grow-1 w-2/3 justify-between border-2 border-solid border-red-600">
         <div>Users:</div>
+        <div>
+          <button className="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none font-medium text-sm rounded-lg px-5 py-2.5 text-center mr-5 ">EDIT</button>
+          <button className="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none font-medium text-sm rounded-lg px-5 py-2.5 text-center mr-5">DELETE</button>
+        </div>
       </div>
     </div>
   );
