0fe00ca791aa9dccab213668a4825a9768a64fd8
Patrick Vuong <patrickv77@gmail.com>
2024-03-01 14:45:57 -0600

    edit and delete button spacing

--- client/components/TabItem.js
+++ client/components/TabItem.js
@@ -7,11 +7,11 @@ const TabItem = () => {
     <div className="flex flex-none h-24 border-2 border-solid border-red-600 rounded-xl my-[1px]">
       <div className="flex items-center justify-center grow-1 w-1/6">[item name]</div>
       <div className="flex items-center justify-center grow-1 w-1/6">[item price]</div>
-      <div className="flex grow-1 w-2/3 justify-between border-2 border-solid border-red-600">
+      <div className="flex grow-1 w-2/3 justify-between">
         <div>Users:</div>
-        <div>
-          <button className="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none font-medium text-sm rounded-lg px-5 py-2.5 text-center mr-5 ">EDIT</button>
-          <button className="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none font-medium text-sm rounded-lg px-5 py-2.5 text-center mr-5">DELETE</button>
+        <div className="flex">
+          <button className="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none font-medium text-sm rounded-lg px-5 py-2.5 border-1 border-solid border-blue-800 mr-1">EDIT</button>
+          <button className="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none font-medium text-sm rounded-lg px-5 py-2.5 border-1 border-solid border-blue-800">DELETE</button>
         </div>
       </div>
     </div>
