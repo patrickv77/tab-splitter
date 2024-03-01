5c557d370cdbe73f9381ef91b30fe67c579962d5
Patrick Vuong <patrickv77@gmail.com>
2024-03-01 14:33:23 -0600

    edit flex box for individual items

--- client/components/TabItem.js
+++ client/components/TabItem.js
@@ -5,10 +5,13 @@ import React from 'react';
 const TabItem = () => {
   return (
     <div className="flex flex-none h-24 border-2 border-solid border-red-600 rounded-xl my-[1px]">
-      this is where the bill elements will live, should be able to add, remove,
-      edit, etc
-      <button className="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none font-medium text-sm rounded-lg px-5 py-2.5 text-center mr-5">EDIT</button>
-      <button className="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none font-medium text-sm rounded-lg px-5 py-2.5 text-center mr-5">DELETE</button>
+      <div className="grow-1 w-1/6">[item name]</div>
+      <div className="grow-1 w-1/6">[item price]</div>
+      <div className="flex grow-1 w-2/3">
+        <button className="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none font-medium text-sm rounded-lg px-5 py-2.5 text-center mr-5">EDIT</button>
+        <button className="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none font-medium text-sm rounded-lg px-5 py-2.5 text-center mr-5">DELETE</button>
+        <div>Users:</div>
+      </div>
     </div>
   );
 };
